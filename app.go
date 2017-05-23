d205ea27f21e7f9ec997a431357d2465fc55bfc0
Alex Ellis <alexellis2@gmail.com>
2017-05-23 12:37:19 +0100

    Enable one-shot for faas.

--- app.go
+++ app.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"time"
-
-	"encoding/json"
-	"io/ioutil"
 
 	"strings"
 
@@ -16,6 +16,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// ExecutorStatus from Jenkins public API
 type ExecutorStatus struct {
 	Computer []struct {
 		DisplayName string `json:"displayName"`
@@ -23,28 +24,16 @@ type ExecutorStatus struct {
 	} `json:"computer"`
 }
 
-func getExecutorStatus(url string) (ExecutorStatus, error) {
-	var executorStatus ExecutorStatus
-	var err error
-
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/computer/api/json", url), nil)
-	httpClient := http.Client{}
-	res, err := httpClient.Do(req)
-	if err != nil {
-		return executorStatus, err
-	}
-	defer res.Body.Close()
-	resBody, err := ioutil.ReadAll(res.Body)
-	err = json.Unmarshal(resBody, &executorStatus)
-
-	return executorStatus, err
-}
-
 func main() {
 	var jenkinsHost string
 	var pollTimer int
+
+	var oneShot bool
+
 	flag.StringVar(&jenkinsHost, "urls", "", "remote Jenkins URLs - comma separated")
 	flag.IntVar(&pollTimer, "pollDelay", 60, "specifies the delay in seconds between polling")
+	flag.BoolVar(&oneShot, "oneShot", false, "print to stdout and exit")
+
 	flag.Parse()
 
 	if len(jenkinsHost) == 0 {
@@ -58,41 +47,83 @@ func main() {
 	}, []string{"node", "url"})
 
 	prometheus.MustRegister(agentStatus)
+	hosts := getHosts(jenkinsHost)
 
+	if oneShot {
+		collect(hosts, agentStatus)
+
+		req := httptest.NewRequest("GET", "/metrics", nil)
+		w := httptest.NewRecorder()
+
+		var handler http.Handler
+		handler = promhttp.Handler()
+		handler.ServeHTTP(w, req)
+
+		resp := w.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Println(string(body))
+	} else {
+		// Periodically sample build agents.
+		go func() {
+			for {
+				collect(hosts, agentStatus)
+
+				time.Sleep(time.Duration(time.Duration(pollTimer) * time.Second))
+			}
+		}()
+
+		http.Handle("/metrics", promhttp.Handler())
+		log.Fatal(http.ListenAndServe(":9001", nil))
+	}
+}
+
+func collect(hosts []string, agentStatus *prometheus.GaugeVec) {
+	for _, jenkinsHost := range hosts {
+		status, err := getExecutorStatus(jenkinsHost)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, computer := range status.Computer {
+
+			var online float64
+			if computer.Offline == false {
+				online = 1
+			}
+
+			labels := map[string]string{
+				"node": computer.DisplayName,
+				"url":  jenkinsHost,
+			}
+
+			agentStatus.With(labels).Set(online)
+		}
+	}
+}
+
+func getExecutorStatus(url string) (ExecutorStatus, error) {
+	var executorStatus ExecutorStatus
+	var err error
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/computer/api/json", url), nil)
+	httpClient := http.Client{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return executorStatus, err
+	}
+
+	defer res.Body.Close()
+	resBody, err := ioutil.ReadAll(res.Body)
+	err = json.Unmarshal(resBody, &executorStatus)
+
+	return executorStatus, err
+}
+
+func getHosts(jenkinsHost string) []string {
 	var hosts []string
 	parts := strings.Split(jenkinsHost, ",")
 	for _, part := range parts {
 		hosts = append(hosts, strings.Trim(part, " "))
 	}
-
-	// Periodically sample build agents.
-	go func() {
-
-		for {
-			for _, jenkinsHost := range hosts {
-				status, err := getExecutorStatus(jenkinsHost)
-				if err != nil {
-					fmt.Println(err)
-				}
-				for _, computer := range status.Computer {
-
-					var online float64
-					if computer.Offline == false {
-						online = 1
-					}
-
-					labels := map[string]string{
-						"node": computer.DisplayName,
-						"url":  jenkinsHost,
-					}
-
-					agentStatus.With(labels).Set(online)
-				}
-			}
-			time.Sleep(time.Duration(time.Duration(pollTimer) * time.Second))
-		}
-	}()
-
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":9001", nil))
+	return hosts
 }
